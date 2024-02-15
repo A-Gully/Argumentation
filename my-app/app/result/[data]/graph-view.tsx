9e64303d516541dc878cc1bbeb635998719daf75
A-Gully <agallul@yahoo.com>
2024-02-15 14:22:41 +0000

    view the desired graph

---
+++ my-app/app/result/[data]/graph-view.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import React from "react";
+import VisGraph, { Edge, Node } from "react-vis-graph-wrapper";
+import { GraphData } from "./page";
+import { groundedLabelling } from "./grounded-labelling";
+
+type Props = {
+  data: GraphData;
+  grounded: boolean;
+};
+
+const GraphView = (props: Props) => {
+  console.log(props.grounded);
+  let graph = {
+    nodes: props.data.nodes,
+    edges: props.data.edges,
+  };
+  if (props.grounded == true) {
+    console.log(groundedLabelling(props.data.nodes, props.data.edges));
+    graph = {
+      nodes: groundedLabelling(props.data.nodes, props.data.edges),
+      edges: props.data.edges,
+    };
+  }
+
+  const options = {
+    layout: {
+      hierarchical: false,
+    },
+    edges: {
+      color: "#000000",
+    },
+    height: "800px",
+  };
+
+  return <VisGraph options={options} graph={graph} />;
+};
+export default GraphView;
