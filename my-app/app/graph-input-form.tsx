dda462e4dcc4d1c3ef69194ed38ffbdc89e7e9d8
A-Gully <agallul@yahoo.com>
2024-03-26 12:23:24 +0000

    clean up

--- my-app/app/graph-input-form.tsx
+++ my-app/app/graph-input-form.tsx
@@ -1,41 +1,14 @@
 "use client";
 
 import { useState } from "react";
-import { useRouter } from "next/navigation";
-import { NextResponse } from "next/server";
 import { createGraphData } from "./actions";
 
 type Props = {};
 
 const GraphInputForm = (props: Props) => {
-  // const router = useRouter();
   const [formData, setFormData] = useState({
     relations: "",
   });
-
-  // const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-  //   event.preventDefault();
-
-  //   try {
-  //     const response = await fetch("/api/submit-form", {
-  //       method: "POST",
-  //       body: JSON.stringify(formData),
-  //     });
-
-  //     if (response.ok) {
-  //       const body = await response.json();
-  //       const graphData = JSON.stringify(body.data);
-  //       console.log(graphData);
-  //       router.push("/result/" + graphData);
-  //     } else {
-  //       // Handle error
-  //       console.error("Failed to submit form data");
-  //     }
-  //   } catch (error) {
-  //     // Handle network errors
-  //     console.error("Network error occurred:", error);
-  //   }
-  // };
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
